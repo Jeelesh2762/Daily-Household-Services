8edba17f2c24ed57084c25de37e6ad384cc32398
Munaf Divan <divanmunafsalimsha@gmail.com>
2021-02-26 19:21:07 +0530

    CRUD operations

--- testapp/settings.py
+++ testapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp.apps.MyappConfig',
     'loginmodule.apps.LoginmoduleConfig',
+    'firstdbtest.apps.FirstdbtestConfig',
 ]
 
 MIDDLEWARE = [
