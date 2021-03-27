f6a96bc90cd6b83f2df51a3cd9f9080ad877bc54
Munaf Divan <divanmunafsalimsha@gmail.com>
2021-03-27 21:42:19 +0530

    21:42

--- registration/forms.py
+++ registration/forms.py
@@ -5,11 +5,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 
 class CustomerForm(forms.ModelForm):  
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = Customer
         fields = ['username','password','first_name','last_name','address','email','mobile_no','city']
         
 class ServiceProviderForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput)
 	class Meta:
 		model = ServiceProvider
 		fields = ['username','password','first_name','last_name', 'address' ,'cat_id', 'email' ,'mobile_no' ,'city' , 'service_rate' , 's_license' ,'profile_description']
